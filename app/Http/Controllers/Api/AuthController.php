98854754a6b636454ba4762358e1d76c6ff7a6c2
thaiminhtuan135 <tuana11ht@gmail.com>
2023-03-02 00:48:42 +0700

    fix respone data after login

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -87,7 +87,10 @@ class AuthController extends BaseController
         return response()->json([
             'access_token' => $token,
             'token_type' => 'bearer',
-            'expires_in' => auth()->factory()->getTTL() * 60
+            'expires_in' => auth()->factory()->getTTL() * 60,
+            'user_id' => auth()->user()->id,
+            'name' => auth()->user()->fullname,
+            'email' => auth()->user()->email,
         ]);
     }
 }
